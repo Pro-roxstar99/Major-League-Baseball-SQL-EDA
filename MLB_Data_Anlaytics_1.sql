f8ee67702bb30ff5815a012fd4cad2b74c052779
Pro-roxstar99 <nakulsaha21@gmail.com>
2025-11-27 22:24:39 +0530

    Doing some YoY growth EDA

--- MLB_Data_Anlaytics_1.sql
+++ MLB_Data_Anlaytics_1.sql
@@ -21,3 +21,23 @@ from schools s
 inner join school_details sd
 on s.schoolID = sd.schoolID
 limit 10;
+
+
+-- Starting with EDA and checking some data trends
+
+select sal.yearID,
+		sal.teamID,
+        round(sum(sal.salary)/100000, 1) as current_year_salary_expenses,
+        lag(round(sum(sal.salary)/100000, 1))
+			over(partition by sal.teamID order by sal.yearID) as prev_year_salary_expenses,
+		round(
+        (sum(sal.salary) - lag(sum(sal.salary)) over(partition by sal.teamID order by sal.yearID))*100
+        /
+        (lag(sum(sal.salary)) over(partition by sal.teamID order by sal.yearID))
+        ,1) as salary_yoy_growth_pct
+        
+        
+from salaries as sal
+group by 1,2
+order by 2,1
+limit 200;
